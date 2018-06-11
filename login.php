95aae9c88c598feab69bad1333dbcf8d2583fe8c
Kundan Kumar <Kundan_kk52@outlook.com>
2018-06-11 12:27:12 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'db_function.php';
+$db = new Db_Functions();
+
+$response = array("error"=>FALSE);
+
+if (isset($_POST['email']) && isset($_POST['password']))   {
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $user = $db->getUserByEmailAndPassword($email,$password);
+
+    if ($user != false) {
+        $response["error"] = FALSE;
+//        $response["error_msg"] = "No Error! Login Successful";
+        $response["uid"] = $user["unique_id"];
+        $response["user"]["name"] = $user["name"];
+        $response["user"]["email"] = $user["email"];
+        $response["user"]["created_at"] = $user["created_at"];
+        $response["user"]["updated_at"] = $user["updated_at"];
+        echo json_encode($response);
+    }
+    else    {
+        $response["error"] = TRUE;
+        $response["error_msg"] = "Login information are wrong. Please try again";
+        echo json_encode($response);
+    }
+}
+
+else    {
+    $response["error"] = TRUE;
+    $response["error_msg"] = "Required parameters (email,password) is missing";
+    echo json_encode($response);
+}
+
+?>
